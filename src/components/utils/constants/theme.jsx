6a19c3a73d517032fa6fd2dc6bf1b61f1bc8acfb
Yevhenii Pozniak <jekamanu@gmail.com>
2022-10-01 16:34:25 +0300

    add Theming
    Додана темізація

---
+++ src/components/utils/constants/theme.jsx
@@ -0,0 +1,12 @@
+// темізація
+export const theme = {
+  colors: {
+    grey: '#919da9',
+    green: 'green',
+    red: 'red',
+    black: '#000',
+    white: 'white',
+  },
+  // spacing: 4,
+  spacing: value => `${5 * value}px`,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import { ThemeProvider } from '@emotion/react';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { theme } from 'components/utils/constants/theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
